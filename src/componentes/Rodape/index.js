f203dfa8612cb18a33a951094ecc48e80b8073e7
Vinícius de Lima Barbosa <159471546+ViniciusLBarbosa@users.noreply.github.com>
2024-08-26 07:49:38 -0300

    Organo Update
    Added:
    - "new team" option
    - Team color now can be changed
    - Colaborator card can be excluded and added to favorites

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -27,7 +27,7 @@ const Rodape = () => {
        </section>
        <section>
         <p>
-            Desenvolvido por Alura.
+            Desenvolvido por Vinícius Lima.
         </p>
        </section>
     </footer>)
